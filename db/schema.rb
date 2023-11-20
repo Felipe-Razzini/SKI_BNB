95cbde4ab343ec87dd9172cfea6c80d360cc7e51
Christine Matta <christine.onsi@gmail.com>
2023-11-20 15:59:20 +0100

    Create the ski model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_131812) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_144320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "skis", force: :cascade do |t|
+    t.string "brand"
+    t.string "experience_level"
+    t.integer "size"
+    t.integer "daily_price"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
