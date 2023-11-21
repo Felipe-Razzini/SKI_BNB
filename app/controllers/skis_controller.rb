4d5ece0dc864b0a9131df6c482637e31a4758df1
Tom Cassells <tom.cassells14@gmail.com>
2023-11-21 14:56:44 +0100

    View completed

--- app/controllers/skis_controller.rb
+++ app/controllers/skis_controller.rb
@@ -1,6 +1,9 @@
 class SkisController < ApplicationController
-
   def index
     @skis = Ski.all
+  end
+
+  def show
+    @ski = Ski.find
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,3 @@ create_table "skis", force: :cascade do |t|
   t.datetime "created_at", null: false
   t.datetime "updated_at", null: false
   t.string "location"
-
-
