aa4ada3231045785526e30ebbbd5b77c55e55abc
Felipe Razzini <feliperazzini95@gmail.com>
2023-11-21 12:31:23 +0100

    seeds starting point

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,18 @@ create_table "skis", force: :cascade do |t|
   t.datetime "updated_at", null: false
   t.string "location"
 
+  puts 'Cleaning database...'
+  Ski.destroy_all
 
+  puts 'Creating skis...'
+  atomic = { brand: 'Atomic', experience_level: 'Beginner', phone_number: '+55 11 2305-2200', category: 'italian' }
+  rossignol = { brand: 'Rossignol', experience_level: 'Beginner', phone_number: '+31 6 20 40 60', category: 'chinese' }
+  lacroix = { brand: 'Lacroix', experience_level: 'Advanced', phone_number: '+31 6 50 60 70', category: 'belgian' }
+  salomon = { brand: 'Salomon', experience_level: 'Intermediate', phone_number: '+55 11 2495-3431', category: 'french' }
+  fischer = { brand: 'Fischer', experience_level: 'R. Iaiá, 83, São Paulo', phone_number: '+55 11 2339-2072', category: 'japanese' }
+
+  [z_deli, otemba, chun, entrecote, kitchin].each do |attributes|
+    restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+  end
+  puts 'Finished!'
