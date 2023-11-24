a15886185d5ed85bdd7a778f1d5fae11bde89b3a
Felipe Razzini <feliperazzini95@gmail.com>
2023-11-24 11:48:29 +0100

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_132742) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_104729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_132742) do
     t.datetime "updated_at", null: false
     t.string "location"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
